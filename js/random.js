79212599bc104fc93e7e4deffdeaef31301a7f80
zhezheup <2543012055@qq.com>
2023-01-10 21:53:14 +0800

    Site updated: 2023-01-10 21:52:46

---
+++ js/random.js
@@ -0,0 +1,1 @@
+var posts=["posts/f9a964a9/","posts/fd31ca55/","posts/30335c15/","posts/fa1802cb/","posts/6c0c8dda/","posts/9454662e/","posts/7eea43f6/","posts/9c7668f5/","posts/f85532d9/","posts/5c8c37e6/","posts/5c8c37e6/","posts/57b0fba5/","posts/8d1f2d1/"];function toRandomPost(){window.open('/'+posts[Math.floor(Math.random() * posts.length)],"_self");};
